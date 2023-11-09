b892bd90b87e2fdd255c111003a5ab2bc74b6b18
Roman Ortega Mondaca <romtega88@gmail.com>
2023-11-08 22:59:23 -0600

    js and media queries

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
-"use script";
+"use strict";
 
-console.log("hello");
+const form = document.querySelector("#form");
+const inputForm = document.querySelector("#input-form");
+const inputWarning = document.querySelector("#input-warning");
+
+function validEmail() {
+  inputForm.classList.remove("input-warning");
+  inputWarning.classList.add("hidden");
+}
+
+function notValidEmail() {
+  inputWarning.classList.remove("hidden");
+  inputForm.classList.add("input-warning");
+}
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const emailRegex =
+    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  const testEmail = emailRegex.test(inputForm.value.trim());
+
+  if (testEmail) {
+    validEmail();
+  } else {
+    notValidEmail();
+  }
+});
